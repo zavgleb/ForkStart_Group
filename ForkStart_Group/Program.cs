4f1e8acb5486cae63272fac14328f814781e4ce5
Manish Agrawal <manish.agrawal@gmail.com>
2019-01-28 14:30:54 -0500

    Added main method

--- ForkStart_Group/Program.cs
+++ ForkStart_Group/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Diagnostics;
+
+/*
+ A project to help student groups learn how to collaborate using github
+
+  Task:
+  One group member forks the repository, and adds fellow group members as collaborators to the repository.
+  
+  These fellow group members clone the repository and add content. Once all group members are synced, all
+    group members have the same code
+   */
 
 namespace ForkStart_Group
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Debug.WriteLine("Hello World from the instructor!");
+
+      // team member A's content goes here
+
+      // team member B's content goes here
+
+      // team member C's content goes here
+
     }
+  }
 }
